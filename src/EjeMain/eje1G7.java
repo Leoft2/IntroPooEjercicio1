d89075e3a9ba22a5bf6f79c57214be28532ea179
Leoft2 <leonelvalenzuela110@gmail.com>
2023-03-31 22:29:37 -0300

    primer commit atributos-objetos

---
+++ src/EjeMain/eje1G7.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EjeMain;
+
+import Entidad.Persona;
+
+
+
+
+
+
+
+
+public class eje1G7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       // 2) Crear los objetos (hay que importar)
+
+       Persona p1 = new Persona();
+       
+       p1.nombre = "Ignacio";
+       p1.apellido = "Fernandez";
+       p1.edad = 25;
+       
+        System.out.println(p1.nombre + " "+ p1.apellido + " " + p1.edad);
+       
+       
+        
+        
+        
+        
+        
+        
+    }
+
+}
